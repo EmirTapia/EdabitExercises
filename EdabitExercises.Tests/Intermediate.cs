43dde3b9bcd3d29899e7c49f3121e2c8e4038944
etapia <emirtapiamendez@gmail.com>
2022-01-21 16:08:29 -0600

    first commit

---
+++ EdabitExercises.Tests/Intermediate.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+
+namespace EdabitExercises.Tests
+{
+    public class Intermediate
+    {
+        [Test]
+        [TestCase("There is a bomb.", ExpectedResult = "Duck!!!")]
+        [TestCase("Hey, did you find it?", ExpectedResult = "There is no bomb, relax.")]
+        [TestCase("Hey, did you think there is a bomb?", ExpectedResult = "Duck!!!")]
+        [TestCase("This goes boom!!!", ExpectedResult = "There is no bomb, relax.")]
+        [TestCase("Hey, did you find the BoMb?", ExpectedResult = "Duck!!!")]
+        [TestCase("Commotion in the third, a bomb is found!", ExpectedResult = "Duck!!!")]
+        public static string Bomb(string a)
+        {
+            return EdabitExercises.Intermediate.Bomb(a);
+        }
+
+        [Test]
+        [TestCase(1, true, ExpectedResult = false)]
+        [TestCase(0, "0", ExpectedResult = false)]
+        [TestCase(1, 1, ExpectedResult = true)]
+        [TestCase(true, true, ExpectedResult = true)]
+        [TestCase("nmv", "nmv", ExpectedResult = true)]
+        [TestCase(0, "", ExpectedResult = false)]
+        [TestCase(1, "1", ExpectedResult = false)]
+        [TestCase('h', 'm', ExpectedResult = false)]
+        [TestCase('a', 'a', ExpectedResult = true)]
+        [TestCase(0, false, ExpectedResult = false)]
+        [TestCase(1.0, 1.0, ExpectedResult = true)]
+        [TestCase("str", "rts", ExpectedResult = false)]
+        [TestCase('c', "test", ExpectedResult = false)]
+        [TestCase(true, "true", ExpectedResult = false)]
+        [TestCase(true, false, ExpectedResult = false)]
+        public static bool CheckEquality(object a, object b)
+        {
+            return EdabitExercises.Intermediate.CheckEquality(a, b);
+        }
+
+        [Test]
+        public void ParseArray()
+        {
+            object[] testArr_1 = new object[] { 1, 2, "a", "b" };
+            object[] testArr_2 = new object[] { "a", "b", 10, 115 };
+            object[] testArr_3 = new object[] { };
+            object[] testArr_4 = new object[] { -4, "k", 0 };
+            object[] testArr_5 = new object[] { "Hell000!", 5 };
+
+            Assert.AreEqual(new string[] { "1", "2", "a", "b" }, EdabitExercises.Intermediate.ParseArray(testArr_1));
+            Assert.AreEqual(new string[] { "a", "b", "10", "115" }, EdabitExercises.Intermediate.ParseArray(testArr_2));
+            Assert.AreEqual(new string[] { }, EdabitExercises.Intermediate.ParseArray(testArr_3));
+            Assert.AreEqual(new string[] { "-4", "k", "0" }, EdabitExercises.Intermediate.ParseArray(testArr_4));
+            Assert.AreEqual(new string[] { "Hell000!", "5" }, EdabitExercises.Intermediate.ParseArray(testArr_5));
+        }
+
+        [Test]
+        [TestCase("eDaBiT", ExpectedResult = new int[] { 1, 3, 5 })]
+        [TestCase("eQuINoX", ExpectedResult = new int[] { 1, 3, 4, 6 })]
+        [TestCase("determine", ExpectedResult = new int[] { })]
+        [TestCase("STRIKE", ExpectedResult = new int[] { 0, 1, 2, 3, 4, 5 })]
+        [TestCase("sUn", ExpectedResult = new int[] { 1 })]
+        [TestCase("SpiDer", ExpectedResult = new int[] { 0, 3 })]
+        [TestCase("accOmpAnY", ExpectedResult = new int[] { 3, 6, 8 })]
+        [TestCase("@xCE#8S#i*$en", ExpectedResult = new int[] { 2, 3, 6 })]
+        [TestCase("1854036297", ExpectedResult = new int[] { })]
+        [TestCase("Fo?.arg~{86tUx=|OqZ!", ExpectedResult = new int[] { 0, 12, 16, 18 })]
+        public static int[] IndexTests(string str)
+        {
+            return EdabitExercises.Intermediate.IndexOfCapitals(str);
+        }
+    }
+}
---
+++ EdabitExercises/Intermediate.cs
@@ -0,0 +1,40 @@
+﻿namespace EdabitExercises
+{
+    public static class Intermediate
+    {
+        //Create a function that finds the word "bomb" in the given string (not case sensitive).
+        //If found, return "Duck!!!", otherwise, return "There is no bomb, relax.".
+        public static string Bomb(string word) =>
+            word.ToUpper().Contains("BOMB") ? "Duck!!!" : "There is no bomb, relax.";
+
+        //In this challenge, you must verify the equality of two different values given the parameters a and b.
+        //Both the value and type of the parameters need to be equal.The possible types of the given parameters are:
+        //Numbers
+        //Strings
+        //Booleans (false or true)
+        //What have you learned so far that will permit you to do two different checks(value and type) with a single statement?
+        //Implement a function that returns true if the parameters are equal, and false if they are not.
+        public static bool CheckEquality(object a, object b) =>
+            a.Equals(b) ? true : false;
+
+        //Create a function that takes an array of integers and strings, converts integers to strings, and returns the array as a string array.
+        public static string?[] ParseArray(object[] arr) =>
+            Array.ConvertAll(arr, element => element.ToString());
+
+        //Create a function that takes a single string as argument and returns an ordered array containing the indices of all capital letters in the string.
+        public static int[] IndexOfCapitals(string str)
+        {
+            List<int> list = new List<int>();
+            int j = 0;
+            foreach (var item in str)
+            {
+                if(char.IsUpper(item))
+                {
+                    list.Add(j);
+                }
+                j++;
+            }
+            return list.ToArray();
+        }
+    }
+}
